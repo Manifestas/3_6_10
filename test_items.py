1d22fa419da684c572ada7abcc5c72acfd6b9877
m.sukharev <m.sykharev@smfox.ru>
2024-06-15 16:48:38 +0300

    add assert and sleep

--- test_items.py
+++ test_items.py
@@ -7,4 +7,4 @@ def test_product_page_contains_add_cart_button(browser):
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
     time.sleep(30)
     elements = browser.find_elements(By.CSS_SELECTOR, '#add_to_basket_form button[type="submit"]')
-    assert len(elements) == 1
+    assert len(elements) == 1, 'basket button not found'
