fba9118f033c246cc17de0c7612a04b247eb84cf
m.sukharev <m.sykharev@smfox.ru>
2024-06-15 16:46:06 +0300

    add assert and sleep

--- test_items.py
+++ test_items.py
@@ -1,6 +1,10 @@
+import time
+
 from selenium.webdriver.common.by import By
 
 
 def test_product_page_contains_add_cart_button(browser):
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
-    browser.find_element(By.CSS_SELECTOR, '#add_to_basket_form button[type="submit"]')
+    time.sleep(30)
+    elements = browser.find_elements(By.CSS_SELECTOR, '#add_to_basket_form button[type="submit"]')
+    assert len(elements) == 1
